a48d6cc18a94c66aad72e757a4c651f9f29c3170
Anderson de França Queiroz <contato@andersonq.eti.br>
2014-03-15 18:45:19 +0000

    Configuration file, to keep mysql conf and etc

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * All configuration will be defined here
+ */
+
+/* MySQL conf */
+// The name of the database
+define('DB_NAME', 'ixb13128');
+// MySQL database username
+define('DB_USER', 'ixb13128');
+// MySQL database password
+define('DB_PASSWORD', 'tchingso');
+// MySQL hostname
+define('DB_HOST', 'devweb2013.cis.strath.ac.uk');
+//Prefix to all tables
+$table_prefix = 'ind_';
+?>
