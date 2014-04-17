92c06f74bd25030c136a04d07f18c8650e1f825d
Mario Cecchi <macecchi@gmail.com>
2014-04-18 00:52:08 +0100

    config sample

--- config.sample.php
+++ config.sample.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'indicia');
 // MySQL database username
 define('DB_USER', 'root');
 // MySQL database password
-define('DB_PASSWORD', 'slhqYsuu');
+define('DB_PASSWORD', 'your_password');
 // MySQL hostname
 define('DB_HOST', '127.0.0.1');
 //Prefix to all tables
