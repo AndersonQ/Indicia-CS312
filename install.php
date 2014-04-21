49b9763da585b4ebb4f31944df8f7ad7eebe2fbe
Anderson de França Queiroz <contato@andersonq.eti.br>
2014-04-21 21:51:57 +0100

    Fixing bug

--- install.php
+++ install.php
@@ -22,35 +22,56 @@ echo "Connected to database.<br>Creating tables...<br>";
 /* Create tables statements */
 //User credentials
 $create = <<<SQL
-DROP TABLE IF EXISTS `{$table_prefix}pictures`;
-
-CREATE TABLE `{$table_prefix}pictures` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `picture` varchar(255) NOT NULL DEFAULT '',
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `ind_pictures_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `ind_user_credentials` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `{$table_prefix}user_credentials`;
-
-CREATE TABLE `{$table_prefix}user_credentials` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `pass` varchar(64) NOT NULL,
-  `salt` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS {$table_prefix}user_credentials;
 SQL;
 
-/* Creating tables */
+/* Creating table */
 $res = $db->query($create);
 //check if table was created
 if (!$res)
-	die("Error whilie creating 'user_credentials' table: " . $db->error);
+	die("Error whilie deleting table {$table_prefix}user_credentials" . $db->error);
+
+$create = <<<SQL
+CREATE TABLE {$table_prefix}user_credentials (
+    id int(11) unsigned NOT NULL AUTO_INCREMENT,
+    email varchar(100) NOT NULL,
+    pass varchar(64) NOT NULL,
+    salt varchar(64) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY email (email)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SQL;
+/* Creating table */
+$res = $db->query($create);
+//check if table was created
+if (!$res)
+	die("Error whilie creating table {$table_prefix}user_credentials" . $db->error);
+
+$create = <<<SQL
+DROP TABLE IF EXISTS {$table_prefix}pictures;
+SQL;
+/* Creating table */
+$res = $db->query($create);
+//check if table was created
+if (!$res)
+	die("Error whilie deleting table {$table_prefix}pictures" . $db->error);
+
+$create = <<<SQL
+CREATE TABLE {$table_prefix}pictures (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  user_id int(10) unsigned NOT NULL,
+  picture varchar(255) NOT NULL DEFAULT "",
+  date datetime NOT NULL,
+  PRIMARY KEY (id),
+  KEY user_id (user_id),
+  CONSTRAINT ind_pictures_ibfk_1 FOREIGN KEY (user_id) REFERENCES ind_user_credentials (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SQL;
+/* Creating table */
+$res = $db->query($create);
+//check if table was created
+if (!$res)
+	die("Error whilie creating table {$table_prefix}pictures: " . $db->error);
 
 echo "Tables created.<br><br>Installation complete.";
 
